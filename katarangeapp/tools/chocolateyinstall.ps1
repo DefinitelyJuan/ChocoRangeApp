dba59e0e9daac354f51b78e9bf420363afd2117d
zolanovich <avilajuan0301@gmail.com>
2022-07-04 23:29:35 -0400

    Choco files

---
+++ katarangeapp/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$name = 'katarangeapp'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$FileLocation = Join-Path $toolsDir "rangeconsoleapp.exe"
+
+Install-BinFile -name $name -path $FileLocation
